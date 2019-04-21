a2ddb676e38120f9e84ac8f9ea13a898390adac3
Ashok Dudhade <ashok.dudhade@outlook.com>
2019-04-20 21:08:43 -0500

    Updates

---
+++ precache-manifest.61f2ccf709726f4802662558d96647ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a62e2d7b50c95367ec51",
+    "url": "/static/js/main.57374822.chunk.js"
+  },
+  {
+    "revision": "e8b366e21090c1d537b5",
+    "url": "/static/js/2.65f736ac.chunk.js"
+  },
+  {
+    "revision": "a62e2d7b50c95367ec51",
+    "url": "/static/css/main.f77714ea.chunk.css"
+  },
+  {
+    "revision": "e8b366e21090c1d537b5",
+    "url": "/static/css/2.a81c94b2.chunk.css"
+  },
+  {
+    "revision": "5b2889e68b91e97f0e794ef9e73eaed3",
+    "url": "/index.html"
+  }
+];
